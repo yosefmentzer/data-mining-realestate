165f29f299e7799d605f410d96f18999d37e3251
Yosef Mentzer <yosef.mentzer@gmail.com>
2022-07-13 01:08:06 +0300

    second commit. scraper works. gitignore updated.

--- realestatescraper.py
+++ realestatescraper.py
@@ -3,19 +3,20 @@ import requests
 import logging
 import sys
 import pandas as pd
+import urllib.parse
 
 # constants
-YAD2 = 'https://www.yad2.co.il/realestate/forsale?topArea=19&area=42&city=8700'
-MADLAN = 'https://www.madlan.co.il/'
-
 CLASS = 'tblModaa round6 shadowPres CardStyle tblModaaArrow'
 FLAVOR = 'bs4'
 
-KOMO = 'https://www.komo.co.il/code/nadlan/apartments-for-sale.asp?nehes=1&cityName=%D7%A8%D7%A2%D7%A0%D7%A0%D7%94'
-KOMO = 'https://www.komo.co.il/code/nadlan/apartments-for-sale.asp?nehes=1&cityName=%D7%AA%D7%9C+%D7%90%D7%91%D7%99%D7%91+%D7%99%D7%A4%D7%95'
-KOMO = 'https://www.komo.co.il/code/nadlan/apartments-for-sale.asp?nehes=1&cityName=%D7%99%D7%A8%D7%95%D7%A9%D7%9C%D7%99%D7%9D'
-KOMO = 'https://www.komo.co.il/code/nadlan/apartments-for-sale.asp?nehes=1&cityName=%D7%97%D7%99%D7%A4%D7%94'
-KOMO = 'https://www.komo.co.il/code/nadlan/apartments-for-sale.asp?nehes=1&cityName=%D7%A8%D7%9E%D7%AA+%D7%92%D7%9F'
+KOMO = 'https://www.komo.co.il'
+KOMO_LIST = 'https://www.komo.co.il/code/nadlan/quick-links.asp?nehes=1&subLuachNum=2'
+SALE_URL_SUBSTRING = '/code/nadlan/apartments-for-sale.asp'
+TAIL_SALE_URL_SUBSTRING = '?nehes=1&cityName='
+
+PAGE_SUBSTRING = '?currPage'
+HEAD_PAGE_URL = 'https://www.komo.co.il/code/nadlan/apartments-for-sale.asp?currPage='
+TAIL_PAGE_URL = '&subLuachNum=2&nehes=1&cityName='
 
 # logger setup
 logger = logging.getLogger('scraper')
@@ -60,18 +61,177 @@ def parse_response(r):
     return soup
 
 
+def get_citynames(soup):
+    """
+
+    :param soup:
+    :return:
+    """
+    citynames = []
+    for link in soup.find_all('a'):
+        if SALE_URL_SUBSTRING in link.get('href'):
+            if link.get('cityname'):
+                citynames.append(link.get('cityname'))
+
+    return citynames
+
+
+def get_urls(citynames):
+    """
+
+    :param citynames:
+    :return:
+    """
+    cities_urls = []
+    for cityname in citynames:
+        cityname_quoted = urllib.parse.quote(cityname)  # Hebrew letters must be quoted
+        url = ''.join([KOMO, SALE_URL_SUBSTRING, TAIL_SALE_URL_SUBSTRING, cityname_quoted])
+        cities_urls.append((cityname, cityname_quoted, url))
+
+    return cities_urls
+
+
+def get_pages(soup):
+    """
+
+    :param soup:
+    :return:
+    """
+    ls = []
+    for href in soup.find_all('a'):
+        if 'currPage' in href.get('href'):
+            ls.append(href.string)
+    pages = [elt for elt in ls if elt.isnumeric()]
+
+    return pages
+
+
+def get_url_for_page(page, cityname):
+    """
+
+    :param page:
+    :param cityname:
+    :return:
+    """
+    page_url = ''.join([HEAD_PAGE_URL, page, TAIL_PAGE_URL, cityname])
+
+    return page_url
+
+
+def get_df_from_url(url, cityname):
+    """
+
+    :param url:
+    :return:
+    """
+    dfs = pd.read_html(url, attrs={'class': CLASS}, flavor=FLAVOR)
+    df_list = [df[0] for df in dfs]
+    df = pd.DataFrame(df_list)
+    df = df.reset_index()
+    df = df.drop(columns=['index', 4])
+    df.columns = ['pictures', 'description1', 'price', 'description2']
+    df['cityname'] = cityname
+    df = df[['cityname', 'pictures', 'description1', 'price', 'description2']]
+
+    return df
+
+
+def print_df(df, cityname):
+    """
+
+    :param df:
+    :return:
+    """
+    print(f'city name: {cityname}')
+    for i in range(len(df)):
+        print(f'Record number: {i+1} for city {cityname}')
+        for col in df.columns[1:]:
+            print(col, ': ', df.iloc[i][col])
+        print('\n')
+
+
 def main():
-    # Part 1
-    r = get_response(KOMO)
+
+    r = get_response(KOMO_LIST)
     if r:
         logger.info(f'Successful response.{r.url}')
     else:
         logger.error(f'Request error: {r}.{r.url}')
         return
-    # soup = parse_response(r)
-    # df_list = pd.read_html(soup.get_text())
-    # tables = soup.find_all('table')
-    dfs = pd.read_html(KOMO, attrs={'class': CLASS}, flavor=FLAVOR)
+    soup = parse_response(r)
+    if soup:
+        logger.info(f'Successful parsing.{r.url}')
+    else:
+        logger.error(f'Parsing error. {r.url}')
+    citynames = get_citynames(soup)
+    if citynames:
+        logger.info(f'Got {len(citynames)} citynames')
+    else:
+        logger.error('Error getting citynames.')
+    cities_urls = get_urls(citynames)
+    if cities_urls:
+        logger.info(f'Got {len(cities_urls)} cities_urls')
+    else:
+        logger.error('Error getting cities_urls.')
+
+    cities_df_list = []
+    for cityname, cityname_quoted, url in cities_urls:
+        print(f'Loading data for city: {cityname}...')
+        df_list = []
+        df = pd.DataFrame()
+        try:
+            df = get_df_from_url(url, cityname)
+            logger.info(f'Got df from {url} with {len(df)} rows.')
+        except Exception as e:
+            logger.error(f'Error getting df from {url}. {str(e)}')
+            continue
+
+        if not df.empty:
+            df_list.append(df)
+
+        r = get_response(url)
+        if r:
+            logger.info(f'Successful response.{r.url}')
+        else:
+            logger.error(f'Request error: {r}.{r.url}')
+            continue
+
+        soup = parse_response(r)
+        if soup:
+            logger.info(f'Successful parsing.{r.url}')
+        else:
+            logger.error(f'Parsing error. {r.url}')
+
+        pages = get_pages(soup)
+        if pages:
+            logger.info(f'Got pages for {url}')
+
+        for page in pages:
+            try:
+                page_url = get_url_for_page(page, cityname_quoted)
+                logger.info(f'Got url for page {page}.')
+            except Exception as e:
+                logger.error(f'Error getting url for page {page}. {str(e)}')
+                continue
+            df = pd.DataFrame()
+            try:
+                df = get_df_from_url(page_url, cityname)
+                logger.info(f'Got df for page {page}.')
+            except Exception as e:
+                logger.error(f'Error getting df for page {page}. {str(e)}')
+
+            if not df.empty:
+                df_list.append(df)
+
+        # now we have city_df
+        city_df = pd.concat(df_list)
+        print_df(city_df, cityname)
+        cities_df_list.append(city_df)
+    cities_df = pd.concat(cities_df_list)
+
+    logger.info(f'Finished scraping.')
+
+    cities_df.to_csv('realestate_data.csv')
 
 
 if __name__ == '__main__':
